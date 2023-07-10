b9cc6019e7f7b5ebe225e6c420e558a538723281
jessicagrayson <grayjessica12@hotmail.com>
2023-07-10 20:56:48 +0000

    Completed javascript-primitives-and-variables exercise

---
+++ javascript-primitives-and-variables/main.js
@@ -0,0 +1,19 @@
+const fullName = 'Jessica Grayson';
+console.log('value of fullName:', fullName);
+console.log('typeof fullName:', typeof fullName);
+
+const isCool = true;
+console.log('value of isCool', isCool);
+console.log('typeof isCool:', typeof isCool);
+
+const totalPets = 3;
+console.log('value of totalPets:', totalPets);
+console.log('typeof totalPets:', typeof totalPets);
+
+const unknown = null;
+console.log('value of unknown:', unknown);
+console.log('typeof unknown:', typeof unknown);
+
+let never;
+console.log('value of never:', never);
+console.log('typeof never:', never);
