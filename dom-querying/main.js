668a6e16f65df836a8ee819e36191317fa10cc54
jessicagrayson <grayjessica12@hotmail.com>
2023-07-24 18:44:47 +0000

    complete dom-query exercise

---
+++ dom-querying/main.js
@@ -0,0 +1,16 @@
+console.log('hello, world');
+
+const queryH1 = document.querySelector('h1');
+console.log(queryH1);
+
+const queryClass = document.querySelector('#explanation');
+console.log(queryClass);
+
+const queryHint = document.querySelector('.hint');
+console.log(queryHint);
+
+const queryP = document.querySelectorAll('p');
+console.log(queryP);
+
+const queryExample = document.querySelectorAll('.example-link');
+console.log(queryExample);
