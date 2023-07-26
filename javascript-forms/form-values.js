fda36d920579aff5c86dcf9500a1a190ecb7308f
jessicagrayson <grayjessica12@hotmail.com>
2023-07-26 19:54:14 +0000

    completed javascript-forms

---
+++ javascript-forms/form-values.js
@@ -0,0 +1,16 @@
+const $contactForm = document.querySelector('#contact-form');
+
+$contactForm.addEventListener('submit', submitFunction);
+
+function submitFunction(event) {
+  event.preventDefault();
+
+  const $formData = {
+    name: $contactForm.elements.name.value,
+    email: $contactForm.elements.email.value,
+    message: $contactForm.elements.message.value,
+  };
+
+  console.log($formData);
+  document.getElementById('contact-form').reset();
+}
